63a056a10e0461246cd15abe3f4c5f37292d093f
GUILHERME FERREIRA <guilherme.ferreira.30483@esgc.pt>
2024-09-25 22:50:01 +0100

    Components - Main (some tests)

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,3 @@
+<x-layout-guest page-title="Login">
+    <p class="display-6 text-center my-5">Formulario de Login</p>
+</x-layout-guest>
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,5 @@
 <?php
 
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    echo "RH MANGNT";
-});
-
-Route::get('/admin', function () {
-
-    $admin = User::with('detail', 'department')->find(1);
-    dd($admin->toArray());
-});
+Route::View('/login', 'auth.login');
