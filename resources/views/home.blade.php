553ee52046160e6fd73b6bc0985d2c1e45916020
GUILHERME FERREIRA <guilherme.ferreira.30483@esgc.pt>
2024-10-08 10:31:03 +0100

    gate admin

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,7 @@
 <x-layout-app page-title="Home">
 
-    <h1 class="text-center my-5">DENTRO DA APP</h1>
+    @can('admin')
+        <h3 class="text-center mt-5">É o usuario admin que esta logado!</h3>
+    @endcan
 
 </x-layout-app>
